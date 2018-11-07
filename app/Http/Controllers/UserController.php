f7914cc4ea2e68633e13be011ed19c5585cac00b
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2018-11-07 12:22:52 +0100

    Prevent always creating new users
    By getting existing when username passed in request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,11 +12,15 @@ class UserController extends Controller
      * Create a new user with a random name and return it
      * @return new user object as json
      */
-    public function store()
+    public function store(Request $request)
     {
-        // create a new user instance.
-        // name of the user is just a hashed string
-        $user = User::create(['name' => str_random(20)]);
+        if ($request->has('username') && !is_null($request->username)) {
+            $user = User::where(['name' => $request->username])->first();
+        } else {
+            // create a new user instance.
+            // name of the user is just a hashed string
+            $user = User::create(['name' => str_random(20)]);
+        }
 
         return response()->json(compact('user'));
     }
