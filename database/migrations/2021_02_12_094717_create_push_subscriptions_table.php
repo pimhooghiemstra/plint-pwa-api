dec29e6db20e01c32bcfa75932de49b5b87d5ac9
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2021-02-12 09:50:30 +0000

    Create table push_notifications again for updated version of package

---
+++ database/migrations/2021_02_12_094717_create_push_subscriptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePushSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::drop('push_subscriptions');
+        
+        Schema::create('push_subscriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->morphs('subscribable');
+            $table->string('endpoint', 500)->unique();
+            $table->string('public_key')->nullable();
+            $table->string('auth_token')->nullable();
+            $table->string('content_encoding')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('push_subscriptions');
+    }
+}
